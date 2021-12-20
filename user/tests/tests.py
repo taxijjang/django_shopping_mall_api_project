2f7b854394cb6e2cd0b43ecdf260c6b1b1863ecc
taxijjang <gw9122@naver.com>
2021-12-20 22:37:07 +0900

    requirements 환경 분리 및 연습 테스트 코드 작성

--- user/tests/tests.py
+++ user/tests/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from ..models import User
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self) -> None:
+        User.objects.create_user(
+            name='test user',
+            email='test@test.com',
+            password='test password',
+        )
+
+    def test_user_model_기본(self):
+        self.assertEqual(True, True)
