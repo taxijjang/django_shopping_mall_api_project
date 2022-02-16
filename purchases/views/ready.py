e97b5489c11a8f2bf1ccfab52115f54788ff4f48
taxijjang <gw9122@naver.com>
2022-02-16 22:49:55 +0900

    Add kakao_ready swagger 추가

--- purchases/views/ready.py
+++ purchases/views/ready.py
@@ -1,5 +1,8 @@
 import requests
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 
+from rest_framework import status
 from rest_framework import decorators
 from rest_framework.response import Response
 
@@ -7,6 +10,38 @@ from kakaopay.payment import KakaoPayClient
 from products.models import Product
 
 
+
+@swagger_auto_schema(
+    method='post',
+    operation_description='카카오페이로 상품 구매 준비',
+    manual_parameters=[
+        openapi.Parameter(
+            'Authorization',
+            openapi.IN_HEADER,
+            required=True,
+            description="Bearer {token}",
+            type=openapi.TYPE_STRING
+        ),
+    ],
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'product_pk': openapi.Schema(type=openapi.TYPE_INTEGER),
+            'quantity': openapi.Schema(type=openapi.TYPE_INTEGER),
+        }
+    ),
+    responses={
+        status.HTTP_200_OK: openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'next_redirect_pc_url': openapi.Schema(
+                    type=openapi.TYPE_STRING,
+                    format='https://mockup-pg-web.kakao.com/v1/xxxxxxxxxx/info'
+                )
+            }
+        )
+    }
+)
 @decorators.api_view(http_method_names=['POST'])
 def kakaopay_ready(request):
     kakaopay = KakaoPayClient()
