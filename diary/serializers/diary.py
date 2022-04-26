2a7d14858bc58cd80875131ca31262344d5b22a3
taxijjang <gw9122@naver.com>
2022-04-26 21:38:04 +0900

    date로 필드 이름 변경 및 밀리세컨으로 시간 제공

--- diary/serializers/diary.py
+++ diary/serializers/diary.py
@@ -4,10 +4,15 @@ from ..models import Diary
 
 
 class DiarySerializer(serializers.ModelSerializer):
+    date = serializers.SerializerMethodField()
+
     class Meta:
         model = Diary
-        fields = ["id", "content", "emotion", "created_at", ]
+        fields = ["id", "content", "emotion", "date", ]
         extra_kwargs = {
             "id": {"read_only": True},
-            "created_at": {"read_only": True},
+            "date": {"read_only": True},
         }
+
+    def get_date(self, obj):
+        return int(obj.date.utcnow().timestamp() * 1e3)
