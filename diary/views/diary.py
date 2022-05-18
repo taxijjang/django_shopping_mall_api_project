d9e3202f8fb19f230143e0075966bb8bd0ecc235
taxijjang <gw9122@naver.com>
2022-05-18 17:56:13 +0900

    다이어이 권한 allowany

--- diary/views/diary.py
+++ diary/views/diary.py
@@ -1,16 +1,18 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from ..models import Diary
 from ..serializers import DiarySerializer
 
 
 class DiaryListCreateView(generics.ListCreateAPIView):
+    permission_classes = [permissions.AllowAny,]
     queryset = Diary.objects.all()
     serializer_class = DiarySerializer
     pagination_class = None
 
 
 class DiaryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.AllowAny,]
     http_method_names = ['patch', 'delete']
     queryset = Diary.objects.all()
     serializer_class = DiarySerializer
