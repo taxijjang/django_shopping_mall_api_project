b20d06bb78a6e9cd67401e73586c7003f177f9ec
taxijjang <gw9122@naver.com>
2022-07-17 01:07:52 +0900

    get paginated response schema 추가

--- core/paginations.py
+++ core/paginations.py
@@ -33,23 +33,31 @@ class CustomPagination(PageNumberPagination):
             ('data', data),
         )))
 
-
-# class CustomPaginatorInspectorClass(PaginatorInspector):
-#     def get_paginated_response(self, paginator, response_schema):
-#         """
-#         :param BasePagination paginator: the paginator
-#         :param openapi.Schema response_schema: the response schema that must be paged.
-#         :rtype: openapi.Schema
-#         """
-#         return openapi.Schema(
-#             type=openapi.TYPE_OBJECT,
-#             properties=OrderedDict((
-#                 ('next', openapi.Schema(type=openapi.TYPE_STRING, format=openapi.FORMAT_URI, description='다음 페이지 주소')),
-#                 ('previous',
-#                  openapi.Schema(type=openapi.TYPE_STRING, format=openapi.FORMAT_URI, description='이전 페이지 주소')),
-#                 ('page', openapi.Schema(type=openapi.TYPE_INTEGER, description='현재 페이지 번호')),
-#                 ('size', openapi.Schema(type=openapi.TYPE_INTEGER, description='한 페이지에 포함되는 아이템 수')),
-#                 ('data', response_schema),
-#             )),
-#             required=['next', 'previous', 'page', 'size', 'data', ],
-#         )
+    def get_paginated_response_schema(self, schema):
+        return {
+            'type': 'object',
+            'properties': {
+                'page_data': {
+                    'type': 'object',
+                    'properties': {
+                        'next': {
+                            'type': 'string',
+                            'example': 'http://211.202.74.84/api/conveniences/products/?page=3'
+                        },
+                        'previous': {
+                            'type': 'string',
+                            'example': "http://211.202.74.84/api/conveniences/products/?page=1"
+                        },
+                        'page': {
+                            'type': 'integer',
+                            'example': 1
+                        },
+                        'size': {
+                            'type': 'integer',
+                            'example': 10,
+                        },
+                    },
+                },
+                'data': schema
+            }
+        }
