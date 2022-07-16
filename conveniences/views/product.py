10ccea5163c3267113d597d1032b58a12aad4048
taxijjang <gw9122@naver.com>
2022-07-17 00:41:19 +0900

    즐겨찾기, 정렬 parameter 추가

--- conveniences/views/product.py
+++ conveniences/views/product.py
@@ -2,7 +2,7 @@ from django.utils import timezone
 from django.db.models import Q
 
 from rest_framework import generics, permissions
-from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiTypes, inline_serializer
 
 from ..models import Product
 from ..serializers import ProductListSerializer
@@ -37,6 +37,7 @@ class ProductListView(generics.ListAPIView):
         order = self.request.GET.get("order")
         year = self.request.GET.get("year", timezone.now().year)
         month = self.request.GET.get("month", timezone.now().month)
+        favorites = self.request.query_params.getlist('favorites')
         if store:
             queryset = queryset.filter(store=store)
         if title:
@@ -44,12 +45,20 @@ class ProductListView(generics.ListAPIView):
 
         if sale_type:
             queryset = queryset.filter(sale_type=sale_type)
+
+        if favorites:
+            favorite_list = favorites[0].split(',')
+            result_favorite = Q()
+            for favorite in favorite_list:
+                result_favorite |= Q(title=favorite)
+            queryset = queryset.filter(result_favorite)
         # 날짜 필터링
         queryset = queryset.filter(
             Q(year=year)
             & Q(month=month)
         )
-
+        if order:
+            queryset = queryset.order_by(order)
         return queryset
 
     @extend_schema(
@@ -61,32 +70,55 @@ class ProductListView(generics.ListAPIView):
                 OpenApiTypes.STR,
                 OpenApiParameter.QUERY,
                 description="편의점 종류",
+                enum=["cu", "emart24", "seven_eleven", "gs25"],
             ),
             OpenApiParameter(
                 "title",
                 OpenApiTypes.STR,
                 OpenApiParameter.QUERY,
-                description="검색하려는 상품 이름",
+                description="검색하려는 상품 이름\n\nex) 상품이름1",
+            ),
+            OpenApiParameter(
+                "favorites",
+                OpenApiTypes.STR,
+                OpenApiParameter.QUERY,
+                description="즐겨 찾는 상품 목록\n\nex) 상품이름1,상품이름2",
             ),
             OpenApiParameter(
                 "sale_type",
                 OpenApiTypes.STR,
                 OpenApiParameter.QUERY,
                 description="할인 타입",
+                enum=["1+1", "2+1", "3+1", "4+1", "할인상품", "증정상품"],
+            ),
+            OpenApiParameter(
+                "order",
+                OpenApiTypes.STR,
+                OpenApiParameter.QUERY,
+                description="정렬 방법",
+                enum=["price", "-price", ]
             ),
             OpenApiParameter(
                 "year",
                 OpenApiTypes.STR,
                 OpenApiParameter.QUERY,
-                description="할인 연도",
+                description="할인 연도\n\nex) 2022",
             ),
             OpenApiParameter(
                 "month",
                 OpenApiTypes.STR,
                 OpenApiParameter.QUERY,
-                description="할인 월",
+                description="할인 월\n\nex) 1",
             ),
         ],
+        # responses={
+        #     200: inline_serializer(
+        #         name="conveniences store api response",
+        #         fields={
+        #             "page_data":
+        #         }
+        #     )
+        # }
     )
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
